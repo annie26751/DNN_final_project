d66d85cab3fad2f5ca0d0694c8adf097c46393af
annie26751 <105311261+annie26751@users.noreply.github.com>
2024-12-12 22:27:41 +0900

    Add files via upload

---
+++ Seg2Former/config.py
@@ -0,0 +1,17 @@
+ALL_CLASSES = ['background', 'damage']
+
+LABEL_COLORS_LIST = [
+    (0, 0, 0), # Background.
+    (255, 0, 0),
+]
+
+VIS_LABEL_MAP = [
+    (0, 0, 0), # Background.
+    (255, 0, 0),
+]
+
+
+id2label = {
+    0: "background",
+    1: "damage",
+}
